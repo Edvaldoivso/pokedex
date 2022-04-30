65bc5569671d6609e8e1edf84a2749556d5912c8
Edvaldo <49847797+Edvaldoivso@users.noreply.github.com>
2022-04-29 22:48:48 -0300

    Inicio das req.HttpClientService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -18,7 +18,8 @@ import { PagesModule } from './pages/pages.module';
   imports: [
     PagesModule,
     BrowserModule, //So aparece no modulo principal
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
