143f3460fa30340daa9283d2b4c07cf07d78d7eb
Edvaldo I Souza <edvaldoivosouza@ymail.com>
2022-06-08 22:53:04 -0300

    Criação da Função de Filtro de Pokemon

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailsComponent } from './pages/details/details.component';
 
 const routes: Routes = [
   {
@@ -7,6 +8,7 @@ const routes: Routes = [
     loadChildren: () =>
       import('./pages/pages.module').then((p) => p.PagesModule),
   },
+  
 ];
 
 @NgModule({
